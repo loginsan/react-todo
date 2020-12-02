c116306bec2eb7c20bb8791641963ccafcf1acd8
Ilya Loginov <linsykt@gmail.com>
2020-12-03 01:39:39 +0300

    fix new-task-form

--- src/components/new-task-form/new-task-form.js
+++ src/components/new-task-form/new-task-form.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './new-task-form.css';
 
 const NewTaskForm = ( {label, autofocus = true} ) => {
-    //const autofocusAttr = autofocus ? "autofocus" : "";
     return (
         <input className="new-todo" placeholder={label} autoFocus={autofocus} />
     );
